fada0a51d054183bd611e652fe6664b86d3b4480
Zizou897 <78234083+Zizou897@users.noreply.github.com>
2022-01-06 15:27:06 +0000

    news

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gal@)jcb&ul=@pyr^zc**#1!=(6(0&9hx--)t&ixjex%$6t^h5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://areb-portfolio.herokuapp.com/"]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
